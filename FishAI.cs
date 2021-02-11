fbbfc57122f844f85f23499eb66a7ca91159d08a
O. Kaya <ogulcankaya117@gmail.com>
2021-02-11 04:07:30 +0300

    Initial post.

---
+++ FishAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAI : MonoBehaviour
+{
+    [SerializeField] private float speed;   //max forward move speed of the fish.
+    [SerializeField] private int framesBeforeSwap;  //The fish will slowly rotate left or right. After a random interval between 10 to this value, the fish will reconsider and potentially change the rotation direction.
+    [SerializeField] private float maxZ;    //Bounds of the area the fish are allowed in.
+    [SerializeField] private float minZ;    //Bounds of the area the fish are allowed in.
+    [SerializeField] private float maxX;    //Bounds of the area the fish are allowed in.
+    [SerializeField] private float minX;    //Bounds of the area the fish are allowed in.
+    private int swapframes = 0;
+    bool left = true; //determines direction
+
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move() //the fish move on the x-z plane only on this version.
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Random.Range(speed * 0.8f, speed));
+        if (swapframes < 1)
+        {
+            swapframes = Random.Range(10, framesBeforeSwap);
+            float rand = Random.Range(-1f, 1f);
+            if (rand > 0)
+            {
+                left = false;
+            }
+            else if (rand <= 0)
+            {
+                left = true;
+            }
+        }
+        if (left)
+        {
+            transform.Rotate(0, Random.Range(-0.5f, 0f), 0);
+            swapframes--;
+        }
+        else
+        {
+            transform.Rotate(0, Random.Range(0f, 0.5f), 0);
+            swapframes--;
+        }
+
+        if (transform.position.x > maxX || transform.position.z > maxZ || transform.position.x < minX || transform.position.z < minZ)
+        {
+            transform.Rotate(0, transform.rotation.eulerAngles.y + 180, 0);
+        }
+    }
+}
---
+++ FishSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawn : MonoBehaviour
+{
+    public GameObject fish; //the fish to spawn
+    [SerializeField] private int spawnNo;   //no of fish to spawn
+    [SerializeField] private float maxZ;    //bounds of the "pond"
+    [SerializeField] private float minZ;    //bounds of the "pond"
+    [SerializeField] private float maxX;    //bounds of the "pond"
+    [SerializeField] private float minX;    //bounds of the "pond"
+
+    void Start()
+    {
+        SpawnFish(spawnNo);
+    }
+
+    public void SpawnFish(int n)   //if called with 0 this will spawn the predetermined spawn number of fish, else it'll spawn n fish.
+    {
+        if (n == 0)
+            n = spawnNo;
+        for(int i = 0; i < n; i++)
+        {
+            Instantiate(fish, new Vector3(Random.Range(minX, maxX), 0, Random.Range(minZ, maxZ)), Quaternion.Euler(0, Random.Range(0, 360), 0));
+        }
+    }
+}
